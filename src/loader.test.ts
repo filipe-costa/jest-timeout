d5241a13a87cf5d64dfb09d37e116bc2bce545b1
Filipe Costa <filipegraca1992@gmail.com>
2021-10-11 22:50:01 +0100

    pixijs/jest reproducible demo

--- src/loader.test.ts
+++ src/loader.test.ts
@@ -1,14 +1,16 @@
-import { Loader } from "./loader"
+import { ResourceLoader } from "./loader"
 import mock from "./mock.json"
 import mock1 from "./mock1.json"
 import mock2 from "./mock2.json"
+import pikachuMock from "./pikachuMock.json"
+import charmanderMock from "./charmanderMock.json"
+import pixiMock from "./pixiMock.json"
 
 // @ts-ignore
 global.fetch = jest.fn()
 
-
 describe("Promise", () => {
-  const loader = new Loader()
+  const loader = new ResourceLoader()
 
   beforeEach(() => {
     // @ts-ignore
@@ -20,6 +22,12 @@ describe("Promise", () => {
           return Promise.resolve({json: () => mock1})
         case "resources/mock2.json":
           return Promise.resolve({json: () => mock2})
+        case "resources/pixiMock.json":
+          return Promise.resolve({json: () => pixiMock})
+        case "resources/charmanderMock.json":
+          return Promise.resolve({json: () => charmanderMock})
+        case "resources/pikachuMock.json":
+          return Promise.resolve({json: () => pikachuMock})
         default:
           console.log("oopsie")
       }
@@ -30,13 +38,20 @@ describe("Promise", () => {
     jest.clearAllMocks()
   })
 
-  it("should pass using promise", () => {
+  it("should pass without using pixijs loader", () => {
     return loader.load()
       .then(() => {
         expect(loader.cache).toEqual({
           "hello": "world",
           "hello2": "world2"
         })
+      })
+  })
+
+  it("should pass using pixijs loader", () => {
+    return loader.pixiLoad()
+      .then(() => {
+        expect(Object.values(loader.loader.resources).length).toBe(2)
       })
   })
 })
--- src/loader.ts
+++ src/loader.ts
@@ -1,31 +1,51 @@
+import {Loader} from "pixi.js"
 
 const fetchJSON = (url: string) => {
   return fetch(url)
     .then((res) => res.json())
 }
 
-export class Loader {
-  cache: any = {}
-  load() {
+export class ResourceLoader {
+  loader = new Loader()
+
+  cache: {[index: string]: string} = {}
+
+  load = () => {
     return new Promise((resolve) => {
       fetchJSON("resources/mock.json")
       .then((res) => this.loadAll(res.data))
-      .then((res) => {
-        return new Promise((resolve) => {
-          console.log(res)
-          res.forEach((obj) => {
-            console.log(`obj: ${JSON.stringify(obj)}`)
-            this.cache[obj.name] = obj.value
-          })
-          resolve({})
-        })
-      })
+      .then(this.addToCache)
       .then(resolve)
     })
   }
 
-  loadAll(urls: string[]) {
-    console.log(`urls: ${urls}`)
+  addToCache = (assets: {name: string, value: string}[]) => {
+    return new Promise((resolve) => {
+      assets.forEach((obj) => {
+        // console.log(`obj: ${JSON.stringify(obj)}`)
+        this.cache[obj.name] = obj.value
+      })
+      resolve({})
+    })
+  }
+
+  loadAll = (urls: string[]) => {
+    // console.log(`urls: ${urls}`)
     return Promise.all(urls.map((url) => fetchJSON(url)))
+  }
+
+  pixiLoad(){
+    return new Promise((resolve) => {
+      fetchJSON("resources/pixiMock.json")
+        .then((res) => this.loadAll(res.assets))
+        .then(this.addAssets)
+        .then(resolve)
+    })
+  }
+
+  addAssets = (assets: any[]) => {
+    return new Promise((resolve) => {
+      this.loader.add(assets).load(resolve)
+    })
   }
 }
